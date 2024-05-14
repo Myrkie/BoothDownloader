30b7a248c62404f7cd7aebdf628b4708c7d30e8a
Myrkur <Myrkur@Myrkur.dev>
2024-05-14 16:58:02 -0400

    eat a dick 7zip (maybe ill come back to this later)

--- BoothDownloader/src/Program.cs
+++ BoothDownloader/src/Program.cs
@@ -410,38 +410,21 @@ internal static class BoothDownloader
 
         if (config.Config.AutoZip)
         {
-#if WINDOWS_BUILD
-            var child = progressBar.Spawn(10000, "Zipping", childOptions);
-            var childProgress = new ChildProgressBarProgress(child);
-            if (File.Exists(entryDir + ".7z"))
-            {
-                Console.WriteLine("File already exists. Deleting...");
-                File.Delete(entryDir + ".7z");
-            }
-#endif
-#if LINUX_BUILD
+            
             progressBar.Dispose();
             if (File.Exists(entryDir + ".zip"))
             {
                 Console.WriteLine("File already exists. Deleting...");
                 File.Delete(entryDir + ".zip");
             }
-#endif
-#if WINDOWS_BUILD
-            Utils.CompressDirectory(entryDir.ToString(), entryDir + ".7z", childProgress);
-#endif
-#if LINUX_BUILD
+            
             Console.WriteLine("Zipping!");
             ZipFile.CreateFromDirectory(entryDir.ToString(),entryDir + ".zip");
             Console.WriteLine("Zipped!");
-#endif
             
             Directory.Delete(entryDir.ToString(), true);
         }
         
-#if WINDOWS_BUILD
-        progressBar.Dispose();
-#endif
         #endregion
 
         #region Exit Successfully
