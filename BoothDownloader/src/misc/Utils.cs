3e1710aa47e351c278b41e0e7ef4031183ec0b1e
DubyaDude <ushafiq141@gmail.com>
2024-06-08 19:37:39 -0400

    Remove dead code

--- BoothDownloader/src/misc/Utils.cs
+++ BoothDownloader/src/misc/Utils.cs
@@ -6,59 +6,6 @@ namespace BoothDownloader.misc
 {
     public static class Utils
     {
-        public static void Init7Z()
-        {
-            var resourceName = "BoothDownloader.resources.7z.dll";
-            var tempDirectory = Path.Combine(Path.GetTempPath(), "BoothDownloader");
-            var tempDllPath = Path.Combine(tempDirectory, "7z.dll");
-
-            if (File.Exists(tempDllPath))
-            {
-                SevenZipBase.SetLibraryPath(tempDllPath);
-                return;
-            }
-
-            Directory.CreateDirectory(tempDirectory);
-
-            var assembly = Assembly.GetExecutingAssembly();
-            using (var resourceStream = assembly.GetManifestResourceStream(resourceName))
-            {
-                if (resourceStream == null)
-                {
-                    Console.WriteLine("Error: 7z.dll resource not found.");
-                    return;
-                }
-
-                using var fileStream = File.Create(tempDllPath);
-                resourceStream.CopyTo(fileStream);
-            }
-            SevenZipBase.SetLibraryPath(tempDllPath);
-        }
-
-        public static void CompressDirectory(string sourceDirectory, string destinationArchive, IProgress<double> progress)
-        {
-            var compressor = new SevenZipCompressor
-            {
-                CompressionLevel = CompressionLevel.Ultra,
-                CompressionMethod = CompressionMethod.Lzma2,
-                DirectoryStructure = true,
-                PreserveDirectoryRoot = true,
-                ArchiveFormat = OutArchiveFormat.SevenZip
-            };
-            compressor.Compressing += (_, e) =>
-            {
-                progress.Report(e.PercentDone / 100.0);
-            };
-
-            try
-            {
-                compressor.CompressDirectory(sourceDirectory, destinationArchive);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Exception caught in CompressDirectory: {0}", ex);
-            }
-        }
         public static async Task DownloadFileAsync(string url, string destinationPath, IProgress<double> progress, CancellationToken cancellationToken = default)
         {
             using var httpClient = new HttpClient();
